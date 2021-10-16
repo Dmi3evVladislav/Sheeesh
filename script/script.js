8d22f13b961697cb2faaa60d2b743a4c76632c33
VladozzzLavandozzz <72341764+VladozzzLavandozzz@users.noreply.github.com>
2021-10-16 11:12:21 +0300

    для проекта 1

--- script/script.js
+++ script/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
     $(".bxslider").bxSlider({
        pager: true, //точечки снизу
+       infiniteLoop: true,
+       speed: 500,
+       easing: 'ease-out',
+       mode: 'horizontal',
+       auto: true,
+       pause: 3000
     });
 });
