9dec742d3f92853bb393a7349324418213c5dfcb
VladozzzLavandozzz <72341764+VladozzzLavandozzz@users.noreply.github.com>
2021-12-04 11:40:09 +0300

    Проект5

--- script/script2test.js
+++ script/script2test.js
@@ -5,4 +5,9 @@ console.log('Код браузера:', navigator.appCodeName);
 console.log('Информация версии браузера:', navigator.appVersion);
 console.log('Платформа:', navigator.platform);
 console.log('Включены ли куки:', navigator.cookieEnabled);
+console.log('Геолокация:', navigator.geolocation);
 console.log('Геолокация:', navigator.geolocation.getCurrentPosition);
+console.log('Язык пользователя:', navigator.language);
+console.log('Онлайн?:', navigator.onLine);
+console.log('Доступна ли Java:', navigator.javaEnabled());
+  
