f2798e18e78a184447e8a1ea7c1ed2646c7a68b0
VladozzzLavandozzz <72341764+VladozzzLavandozzz@users.noreply.github.com>
2021-12-04 11:27:19 +0300

    еуые6

---
+++ script/script2test.js
@@ -0,0 +1,8 @@
+console.log('Полные данные:', window.navigator);
+console.log('Короткая информация про браузер и операционную систему пользователя:',navigator.userAgent);
+console.log('Хз для чего, но всегда выводит Gecko:', navigator.product)
+console.log('Код браузера:', navigator.appCodeName);
+console.log('Информация версии браузера:', navigator.appVersion);
+console.log('Платформа:', navigator.platform);
+console.log('Включены ли куки:', navigator.cookieEnabled);
+console.log('Геолокация:', navigator.geolocation.getCurrentPosition);
