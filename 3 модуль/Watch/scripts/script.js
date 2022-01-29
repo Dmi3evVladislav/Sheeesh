0b1a85164fbb283136d1ef246dbc375add437d95
Kirikkiller <Kirick.il@yandex.ru>
2022-01-29 14:38:14 +0300

    НЕ работают деления

--- 3 модуль/Watch/scripts/script.js
+++ 3 модуль/Watch/scripts/script.js
@@ -1,6 +1,36 @@
 let canvas = document.getElementById("watch");
 let context = canvas.getContext("2d");
 
+
+function draw_division(R) {
+    let angle, pX, pY, qX, qY;
+    for (let i = 0; i < 60; i++ ) {
+        
+        angle = i/60 * 2*Math.PI;
+        pX = Math.cos(angle) * R;
+        pY = Math.sin(angle) * R;
+
+        qX = 0.9 * pX;
+        qY = 0.9 * pY;
+
+        pX += R; qY += R;
+        qX += R; pY += R;
+
+        let line = new Path2D();
+        line.moveTo(pX, pY);
+        line.lineTo(pX, pY);
+
+        if (i % 5 == 0) {
+            context.lineWidth = 3;
+        } else {
+            context.lineWidth = 1;
+        }
+
+        context.stroke(line);
+    }
+}
+
+
 function draw_watch() {
     context.clearRect(0, 0, 300, 300);
 
@@ -8,8 +38,11 @@ function draw_watch() {
     let circle = new Path2D();
     circle.arc(R+2, R+2, R, 0, 2*Math.PI); 
     context.stroke(circle);
+
+    draw_division(R);
 }
 
 window.onload = function() {
     draw_watch();
 }
+
