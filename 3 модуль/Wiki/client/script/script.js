c6f643a342a51001252666471665bb4b2044c3ea
VladozzzLavandozzz <72341764+VladozzzLavandozzz@users.noreply.github.com>
2022-03-23 18:19:45 +0300

    fgfg

--- 3 модуль/Wiki/client/script/script.js
+++ 3 модуль/Wiki/client/script/script.js
@@ -5,3 +5,17 @@ let vmWiki = new Vue({
         source: "<h1>Я люблю рисковать</h1>"
     }
 })
+
+$.ajax ({
+    url: "https://ru.wkipedia.org/w/api.php",
+    jsonp: "callback",
+    dataType: "jsonp",
+    data: {
+        action: "query",
+        prop: "extacts", 
+        format: "json",
+        exintro: true,
+        redirects: true,
+        titles: userQuery
+    }
+})
